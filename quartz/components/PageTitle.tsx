a744125597694fbd62e1540cc8dc6ce2c0896158
AvantGardeDreams <timficarra@gmail.com>
2026-05-03 18:42:54 -0400

    fixing missing import

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,6 +1,7 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import { joinSegments } from "../util/path"
 import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
