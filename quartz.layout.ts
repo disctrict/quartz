0810bd0ad4ed4ab1067e3209ca0e0341feeb2f76
AvantGardeDreams <timficarra@gmail.com>
2026-05-03 19:07:41 -0400

    changing graph position

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,12 +17,12 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Graph(),
     Component.ConditionalRender({
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
+    Component.Graph(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
